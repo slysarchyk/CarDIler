0511d10578525210f109f09872e7a8174723f668
slysarchyk <58818048+slysarchyk@users.noreply.github.com>
2020-02-19 22:22:21 +0100

    Version 2.0.2 fixed comited to git.

--- CarDIler/Data/Models/Car.cs
+++ CarDIler/Data/Models/Car.cs
@@ -19,26 +19,29 @@ namespace CarDIler.Data.Models.Car
         public Brand Brand { get; set; }
 
         public Category Category { get; set; }
-        
+        public Fuel Fuel { get; set; }
+
+
         [Range(1900, 2100)]
         public int Year { get; set; }
-        public Fuel Fuel { get; set; }
-        
+
         [Required]
         public double Engine { get; set; }
 
         [Required]
         [Range(0, int.MaxValue)]
         public int Distance { get; set; }
+
         [Required]
         public string Color { get; set; }
+
         [Required]
         public string Vin { get; set; }
-        
+
         [Required]
         [Range(0, double.MaxValue)]
         public double PriceNetto { get; set; }
-        
+
         [Required]
         [Range(0, double.MaxValue)]
         public double PriceBrutto { get; set; }
@@ -52,13 +55,10 @@ namespace CarDIler.Data.Models.Car
         
         public string Date { get; set; }
         public string DateEdit { get; set; }
-
-        //AddByUser
         public string AddByName { get; set; }
         public string AddBySurname { get; set; }
         public string AddByPhoneNumber { get; set; }
         public string AddByPosition { get; set; }
-
         public string CoverPath { get; set; }
 
         public virtual ICollection<CarImages> CarImages { get; set; }
